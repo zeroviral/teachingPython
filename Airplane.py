03f1520fac98b2c72adc5dfeb233bf384914b48c
Marc Tucci <marctucci@macbook-air.mynetworksettings.com>
2022-07-09 00:19:02 -0400

    Additions to airplane

--- Airplane.py
+++ Airplane.py
@@ -1,5 +1,6 @@
 class Airplane:
 
+    maxSpeed = None
     passengerCount = None
 
     def __init__(self, passengers=100):
@@ -7,3 +8,21 @@ class Airplane:
 
     def getCapacity(self):
         return self.passengerCount
+
+    def setCapacity(self, newCount):
+        self.passengerCount = newCount
+
+    def getMaxSpeed(self):
+        return self.maxSpeed
+
+    def setMaxSpeed(self, topSpeed):
+        self.maxSpeed = topSpeed
+
+    def calculateTravelTime(self, odoStart, odoEnd):
+        if odoEnd <= odoStart or odoStart < 0:
+            print("Incorrect odometer range.")
+            return None
+        totalDistance = odoEnd - odoStart
+        travelTime = totalDistance // self.maxSpeed
+        return travelTime
+
--- main.py
+++ main.py
@@ -8,3 +8,13 @@ b52 = B52()
 b52.increaseCapacity(45)
 
 print(b52.getCapacity())
+
+b52.setCapacity(300)
+
+print(b52.getCapacity())
+
+b52.setMaxSpeed(600)
+
+print(b52.getMaxSpeed())
+
+print("%i hours" % b52.calculateTravelTime(89999, 97000))
