50e1e52b5d009044790d9fd7ccd434e4dafd5e80
Travis Khan <travis_khan@apple.com>
2022-07-09 23:43:37 -0500

    Adding functionality to support assigning seats to our passengers

--- Airplane.py
+++ Airplane.py
@@ -1,11 +1,15 @@
+from collections import defaultdict
+
 class Airplane:
 
     maxSpeed = None
     passengerCount = None
-    passengerNames = []
+    passengers = defaultdict()
+    seatsAvailable = None
 
-    def __init__(self, passengers=100):
+    def __init__(self, passengers=100, seatsAvailable=100):
         self.passengerCount = passengers
+        self.seatsAvailable = seatsAvailable
 
     def getCapacity(self):
         return self.passengerCount
@@ -28,8 +32,17 @@ class Airplane:
         return travelTime
 
     def addPassenger(self, name):
-        self.passengerNames.append(name)
+        if self.seatsAvailable == 0:
+            print("No more seats available")
+            return
+
+        self.passengers[name] = self.seatsAvailable
+        self.seatsAvailable -= 1
 
     def listPassengers(self):
-        print(self.passengerNames)
+        print(self.passengers)
 
+    def getPassengerSeatNumber(self, name):
+        returnString = ("Passenger '%s' has seat number: %i" % (name, self.passengers[name]))
+
+        return returnString
