92ae4d1a2226a9d382301ee40e1aefe56283b13f
Jeremy-Nieves <jeremyjnieves@gmail.com>
2022-07-10 00:05:26 -0400

    added functionality for adding and listing passengers

--- main.py
+++ main.py
@@ -4,17 +4,8 @@ from B52 import B52
 proto1 = Airplane(passengers=50)
 print(proto1.getCapacity())
 b52 = B52()
+passenger = "John Smith"
 
-b52.increaseCapacity(45)
+proto1.addPassenger(passenger)
+proto1.listPassengers()
 
-print(b52.getCapacity())
-
-b52.setCapacity(300)
-
-print(b52.getCapacity())
-
-b52.setMaxSpeed(600)
-
-print(b52.getMaxSpeed())
-
-print("%i hours" % b52.calculateTravelTime(89999, 97000))
