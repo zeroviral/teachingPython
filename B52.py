eb79913c61f804f7be4154d0079d7e23e36df3d8
Jeremy-Nieves <jeremyjnieves@gmail.com>
2022-07-13 00:50:28 -0400

    Added Cancel Reservation

--- B52.py
+++ B52.py
@@ -5,4 +5,9 @@ class B52(Airplane):
     def increaseCapacity(self, additional):
         self.passengerCount += additional
 
+    def cancelReservation(self, name):
+        if name in self.passengers:
+            self.passengers[name] = None
+
+
 
